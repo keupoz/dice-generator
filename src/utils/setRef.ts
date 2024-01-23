933d0a1131f145907acbf371e3dd1b9ca4cc3102
Ivan Sokolov <keupoz@gmail.com>
2024-01-24 02:08:07 +0700

    Remove unused code

--- src/utils/setRef.ts
+++
@@ -1,9 +0,0 @@
-import { Ref } from "react";
-
-export function setRef<T>(ref: Ref<T>, value: T) {
-  if (typeof ref === "function") {
-    ref(value);
-  } else if (ref !== null) {
-    (ref as Record<"current", T>).current = value;
-  }
-}
