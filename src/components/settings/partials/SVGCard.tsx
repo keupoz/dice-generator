7b9c91077d82ceaaf4fc59ff9e8f2a63968e4ec9
Ivan Sokolov <keupoz@gmail.com>
2024-10-27 14:33:06 +0700

    Fix SVG preview again

--- src/components/settings/partials/SVGCard.tsx
+++ src/components/settings/partials/SVGCard.tsx
@@ -35,7 +35,7 @@ export const SVGCard: FC<SVGCardProps> = memo(({ info }) => {
     <Card>
       <CardContent className="flex gap-4 p-4">
         <div
-          className="bg-checker rounded-sm shrink-0 [&>*]:size-16"
+          className="bg-checker rounded-sm shrink-0 size-16 [&>*]:size-full"
           dangerouslySetInnerHTML={{
             __html: info.raw,
           }}
