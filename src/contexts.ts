05db85161380d80d309dbb357e16819cba2ca862
Ivan Sokolov <keupoz@gmail.com>
2024-10-31 23:08:50 +0700

    Remove unnecessary contexts file

--- src/contexts.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-export const SettingsSectionContext = createContext({
-  currentSection: null as string | null,
-  setCurrentSection(value: string | null) {
-    console.warn('Empty context', value)
-  },
-})
