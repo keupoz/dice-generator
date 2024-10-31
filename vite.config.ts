c4e5cbbe919c4de00cb5c981fb4bdfa646bc254b
Ivan Sokolov <keupoz@gmail.com>
2024-10-31 14:19:36 +0700

    Add vite-plugin-node-polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import path from 'node:path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ['buffer'],
+    }),
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src'),
