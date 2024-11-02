b4db240f5e948e298dedffba2472be0868c98c0f
Ivan Sokolov <keupoz@gmail.com>
2024-11-02 20:41:10 +0700

    Add fonts to files tab

--- src/components/settings/tabs/FilesTab.tsx
+++ src/components/settings/tabs/FilesTab.tsx
@@ -1,14 +1,24 @@
-import { Text, Title } from '@mantine/core'
+import { Divider, Text, Title } from '@mantine/core'
 import { useAppState } from '~/appState'
+import { FontCard } from '~/components/ui/FontCard'
 import { SVGCard } from '~/components/ui/SVGCard'
 import { SettingsTabContent } from '../SettingsTabContent'
 
 export function FilesTab() {
+  const fonts = useAppState(state => state.userFonts)
   const svgs = useAppState(state => state.userSVGs)
 
   return (
     <SettingsTabContent value="files">
-      <Title order={2}>SVGs</Title>
+      <Title order={3}>Fonts</Title>
+
+      {fonts.length === 0
+        ? <Text c="gray">No fonts loaded</Text>
+        : fonts.map(info => <FontCard key={info.id} info={info} />)}
+
+      <Divider />
+
+      <Title order={3}>SVGs</Title>
 
       {svgs.length === 0
         ? <Text c="gray">No SVGs loaded</Text>
