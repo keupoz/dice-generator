ebc671f1ecfe1289f061ab7fdb0a8b55de4f3a13
Ivan Sokolov <keupoz@gmail.com>
2025-05-24 16:58:59 +0700

    Update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,11 @@ export default antfu({
   isInEditor: false,
   rules: {
     'antfu/curly': ['off'],
-    'curly': ['error', 'all'],
+    'antfu/if-newline': ['off'],
 
+    'curly': ['error', 'multi-line', 'consistent'],
     'style/brace-style': ['error', '1tbs'],
+
+    'style/jsx-one-expression-per-line': ['error', { allow: 'non-jsx' }],
   },
 })
