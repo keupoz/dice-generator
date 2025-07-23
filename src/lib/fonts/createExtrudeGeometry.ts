7b57d5ee6868638e786b56c3e61cf906b948961b
Ivan Sokolov <keupoz@gmail.com>
2025-07-23 18:54:51 +0700

    Change fill rule to NonZero

--- src/lib/fonts/createExtrudeGeometry.ts
+++ src/lib/fonts/createExtrudeGeometry.ts
@@ -1,22 +1,13 @@
-import type { SimplePolygon, Vec2 } from 'manifold-3d'
+import type { Vec2 } from 'manifold-3d'
 import type { Path } from 'three'
-import { ShapeUtils } from 'three'
 import { manifold2cad } from '../converters/manifold2cad'
 import { getManifold } from '../manifold'
 
 export function createExtrudeGeometry(paths: Path[], segments: number) {
   const { CrossSection } = getManifold()
-  const segmentedPaths = paths.map(path => path.getPoints(segments))
 
-  if (ShapeUtils.isClockWise(segmentedPaths.flat())) {
-    for (const path of segmentedPaths) {
-      path.reverse()
-    }
-  }
-
-  const polygons = segmentedPaths.map<SimplePolygon>(path => path.map<Vec2>(({ x, y }) => ([x, y])))
-
-  const crossSection = new CrossSection(polygons)
+  const polygons = paths.map(path => path.getPoints(segments).map<Vec2>(({ x, y }) => ([x, y])))
+  const crossSection = new CrossSection(polygons, 'NonZero')
   const manifold = crossSection.extrude(2, 3)
   const mesh = manifold.getMesh()
 
