4b1a60be632c22582f56a706bfb9e94cb74992b1
Ivan Sokolov <keupoz@gmail.com>
2025-07-25 12:44:26 +0700

    No need to compute the whole slice

--- src/dice/utils/getTargetPoint.ts
+++ src/dice/utils/getTargetPoint.ts
@@ -1,6 +1,5 @@
 import type { Vec3 } from '@jscad/modeling/src/maths/vec3'
 import vec3 from '@jscad/modeling/src/maths/vec3'
-import { slice } from '@jscad/modeling/src/operations/extrusions'
 import { strictAt } from '~/utils/array/strictAt'
 import { centerOfMassOfEdges } from './centerOfMassOfEdges'
 
@@ -18,10 +17,10 @@ export type FaceTarget = IndexTarget | CenterTarget
 export function getTargetPoint(points: Vec3[], target: FaceTarget) {
   switch (target.type) {
     case 'edge': {
-      const { edges } = slice.fromPoints(points)
-      const edge = strictAt(edges, target.index)
+      const vertex0 = strictAt(points, target.index - 1)
+      const vertex1 = strictAt(points, target.index)
 
-      return vec3.lerp(vec3.create(), edge[0], edge[1], 0.5)
+      return vec3.lerp(vec3.create(), vertex0, vertex1, 0.5)
     }
 
     case 'vertex': {
--- src/utils/array/strictAt.ts
+++ src/utils/array/strictAt.ts
@@ -1,5 +1,5 @@
 export function strictAt<T>(arr: readonly T[], index: number): T {
-  const item = arr.at(index)
+  const item = arr.at(index % arr.length)
 
   if (item === undefined) throw new Error(`No item at index ${index}`)
 
