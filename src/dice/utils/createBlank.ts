3c57edd7037bd758f393a3802944b9b838ff061d
Ivan Sokolov <keupoz@gmail.com>
2025-07-28 15:43:34 +0700

    Replace blanks implementation with faster solution

--- src/dice/utils/createBlank.ts
+++ src/dice/utils/createBlank.ts
@@ -1,22 +1,41 @@
 import type { Geom3 } from '@jscad/modeling/src/geometries/types'
 import geom3 from '@jscad/modeling/src/geometries/geom3'
+import poly3 from '@jscad/modeling/src/geometries/poly3'
+import vec3 from '@jscad/modeling/src/maths/vec3'
 import { subtract } from '@jscad/modeling/src/operations/booleans'
-import { hull } from '@jscad/modeling/src/operations/hulls'
-import { scale, translate } from '@jscad/modeling/src/operations/transforms'
-import { geodesicSphere } from '@jscad/modeling/src/primitives'
-
-const baseCursor = geodesicSphere({ frequency: 24 })
+import { strictAt } from '~/utils/array/strictAt'
 
 export function createBlank(base: Geom3, delta: number) {
-  const cursor = scale([delta, delta, delta], baseCursor)
+  const extrudedPolygons = geom3.toPolygons(base).map((polygon) => {
+    const [x, y, z] = poly3.plane(polygon)
+    const normal = vec3.fromValues(x, y, z)
 
-  let result = base
+    vec3.scale(normal, normal, -delta)
 
-  for (const polygon of geom3.toPoints(base)) {
-    const cursors = polygon.map(point => translate(point, cursor))
-    const expandedPolygon = hull(cursors)
-    result = subtract(result, expandedPolygon)
-  }
+    const basePoints = poly3.toPoints(polygon)
+    const extrudedPoints = basePoints.map(point => vec3.add(vec3.create(), normal, point))
+    const faces = [
+      poly3.fromPoints([...extrudedPoints].reverse()),
+      polygon,
+      poly3.fromPoints([
+        strictAt(extrudedPoints, -1),
+        strictAt(extrudedPoints, 0),
+        strictAt(basePoints, 0),
+        strictAt(basePoints, -1),
+      ]),
+    ]
 
-  return result
+    for (let i = 1; i < extrudedPoints.length; i++) {
+      faces.push(poly3.fromPoints([
+        strictAt(extrudedPoints, i - 1),
+        strictAt(extrudedPoints, i),
+        strictAt(basePoints, i),
+        strictAt(basePoints, i - 1),
+      ]))
+    }
+
+    return geom3.create(faces)
+  })
+
+  return subtract(base, extrudedPolygons)
 }
